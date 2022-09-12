e8f5bf588c7260c383a0abf0e087c0570cd83dfa
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-12 18:34:17 +0900

    Solved tasks from Seminar 4

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit){
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]+ "\t");
+    }
+    System.Console.WriteLine();
+}
+
+const int MIN_ELEMENTS = -9;
+const int MAX_ELEMENTS = 9;
